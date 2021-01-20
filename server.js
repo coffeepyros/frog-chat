55f19b5479d1495da0f12049f8c79481bdedba32
Christian Heinrich <coffeepyros@gmail.com>
2021-01-20 14:28:08 +0100

    123134325

--- server.js
+++ server.js
@@ -53,12 +53,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/chat", chatRouter);
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

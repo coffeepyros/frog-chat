1298c0c21a21d31f8afb1de95ea9947e7365c67f
Christian Heinrich <coffeepyros@gmail.com>
2021-01-20 13:23:41 +0100

    xxx

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 var port = normalizePort(process.env.PORT || "5000");
-app.set("port", port);
+// app.set("port", port);
 server.listen(port, () => {
   console.log("Listening to port:", port);
 });
